cfa4c7590f426723e551072ed8a8001776223a6b
Michel Marinho <michel.marinho04@outlook.com>
2023-02-22 14:05:21 -0400

    fix: classname

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import { headercontent } from "../mocks/header";
 
 export default function Header() {
   return (
-    <nav class="p-6 px-8 text-white uppercase">
-      <div class="flex justify-center gap-x-12">
+    <nav className="p-6 px-8 text-white uppercase">
+      <div className="flex justify-center gap-x-12">
         {headercontent.map(({ title }, index) => (
-          <a href="#" class="text-lg font-semibold" key={index}>
+          <a href="#" className="text-lg font-semibold" key={index}>
             {title}
           </a>
         ))}
